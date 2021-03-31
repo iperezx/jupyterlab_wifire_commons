f8adbf2aa0fcf8c9bae65196d1e429913713bd11
iperezx <i3perez@sdsc.edu>
2021-03-31 13:06:44 -0700

    Add upload model to CKAN button to toolbar.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,55 @@
 import {
+  IDisposable
+} from '@phosphor/disposable';
+
+import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-/**
- * Initialization data for the jupyterlab_wifire_commons extension.
- */
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+class UploadToCKANButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+
+    let uploadToCKAN = () => {
+      console.log('Upload model to CKAN.');
+    };
+    
+    let button = new ToolbarButton({
+      className: 'uploadToCKANButton',
+      label: 'Upload Model to CKAN',
+      onClick: uploadToCKAN,
+      tooltip: 'Upload to CKAN'
+    });
+    
+    panel.toolbar.insertItem(10, 'uploadToCKAN', button);
+  
+    return button;
+  }
+}
+function activate(app: JupyterFrontEnd): void {
+  let buttonExtension = new UploadToCKANButtonExtension();
+  app.docRegistry.addWidgetExtension('Notebook', buttonExtension);
+}
+
+
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_wifire_commons:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_wifire_commons is activated!');
-  }
+  activate
 };
+
 
 export default extension;
